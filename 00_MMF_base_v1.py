160154192bea4b717b475c9614de28150bbdd036
couttskate <katecou@pnghs.school.nz>
2022-03-16 13:53:17 +1300

    Create 00_MMF_base_v1.py

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,36 @@
+#kate coutts
+#15march2022
+#Mega Movie Fundraiser
+
+
+#import statements
+
+#functions go here
+
+#''''''''Main Routine '''''''
+
+#Set up dictionaries/lists needed to hold data
+
+#Ask user if they have used the program before and show instructions if necessary
+
+#Loop to get ticket details
+
+    #Get name (can't be blank)
+
+    #Get age (between 12 and 130)
+
+    #Calculate ticket price
+
+    #Loop to ask for snacks
+
+    #Calculate snack price
+
+    #Loop to ask for snacks
+
+    #Calculate snack price
+
+#Ask for payment method (and apply surcharge if necessary)
+
+#Calculate total sales and profit
+
+#Output data to text file
