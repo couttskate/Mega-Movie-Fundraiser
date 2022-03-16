8fbad5cf86d93547789428caa698fa7d6a2b3017
couttskate <katecou@pnghs.school.nz>
2022-03-16 14:15:38 +1300

    Create 01_name_not_blank.py

---
+++ 01_name_not_blank.py
@@ -0,0 +1,18 @@
+# functions go here
+
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+        if response != "":
+            return response
+        else:
+            print("Sorry - this can't be blank.")
+
+#main routine goes here
+
+name = not_blank("Name: ")
+
+
+
