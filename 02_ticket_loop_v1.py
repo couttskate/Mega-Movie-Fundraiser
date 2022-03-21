44d4612a312a4fe04691164961fee59bbc36c8d0
couttskate <katecou@pnghs.school.nz>
2022-03-21 14:54:48 +1300

    Update 02_ticket_loop_v1.py

--- 02_ticket_loop_v1.py
+++ 02_ticket_loop_v1.py
@@ -6,8 +6,14 @@ name = ""
 count = 0
 MAX_TICKETS = 5
 
-while name != "xxx" and count <= MAX_TICKETS:
+while name != "xxx" and count < MAX_TICKETS:
 
     #get details
     name = input("Name: ")
     count += 1
+    print("You have {} seats left".format(MAX_TICKETS - count))
+if count < MAX_TICKETS:
+    print("You have sold {} tickets.There are {} places still avaliable.".format(count, MAX_TICKETS - count))
+
+else:
+    print("You have sold all avaliable tickets.")
