d138228c25196f051f4181bb1324cf96165d34d0
couttskate <katecou@pnghs.school.nz>
2022-03-21 14:34:53 +1300

    Update 02_ticket_loop_v1.py

--- 02_ticket_loop_v1.py
+++ 02_ticket_loop_v1.py
@@ -2,4 +2,12 @@
 
 #initialise loop so that it runs at least once
 
-name = 
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count <= MAX_TICKETS:
+
+    #get details
+    name = input("Name: ")
+    count += 1
