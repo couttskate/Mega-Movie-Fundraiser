660b029cdea478194ed6507dd1a5d62b4f932c87
couttskate <katecou@pnghs.school.nz>
2022-05-02 10:46:02 +1200

    Update 11_tickets_snacks_v1.py

--- 11_tickets_snacks_v1.py
+++ 11_tickets_snacks_v1.py
@@ -3,6 +3,7 @@ import pandas
 # initialise snack lists
 
 all_names = ['Rangi', 'Manaia', 'Talia', 'Arihi', 'Fetu']
+all_tickets = [7.5, 10.5, 10.5, 10.5, 6.5]
 
 popcorn = []
 mms = []
@@ -15,6 +16,7 @@ snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
 # Data Frame Dictionary
 movie_data_dict = {
     'Name': all_names,
+    'Ticket': all_tickets,
     'Popcorn': popcorn,
     'Water': water,
     'Pita Chips': pita_chips,
@@ -22,6 +24,14 @@ movie_data_dict = {
     'Orange Juice': orange_juice
     }
 
+# Cost of each snack
+price_dict = {
+    'Popcorn': 2.5,
+    'Water': 2,
+    'Pita Chips': 4.5,
+    'M&Ms': 3,
+    'Orange Juice': 3.25
+}
 test_data = [
     [[2, 'Popcorn'], [1, 'Pita Chips'], [1, 'Orange Juice']],
     [[]],
@@ -51,13 +61,20 @@ for client_order in test_data:
             add_list[-1] = amount
 
 print()
-print("Popcorn: ", snack_lists[0])
-print("M&Ms:", snack_lists[1])
-print("Pita Chips:", snack_lists[2])
-print("Water:", snack_lists[3])
-print("Orange Juice:", snack_lists[4])
-print()
-# print details
+
+# Create dataframe and set index to name column
 movie_frame = pandas.DataFrame(movie_data_dict)
 movie_frame = movie_frame.set_index('Name')
-print(movie_frame)
+
+# create column called 'Sub Total'
+# fill it with price for snacks and ticket
+
+movie_frame["Sub Total"] = \
+    movie_frame['Ticket'] + \
+    movie_frame['Popcorn']*price_dict['Popcorn'] + \
+    movie_frame['Water']*price_dict['Water'] + \
+    movie_frame['Pita Chips']*price_dict['Pita Chips'] + \
+    movie_frame['M&Ms']*price_dict['M&Ms'] + \
+    movie_frame['Orange Juice'] + \
+    movie_frame['Ticket'] + \
+
