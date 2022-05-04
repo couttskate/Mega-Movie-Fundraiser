81068a711ecf83b73036f70a518f984a7dda2b5a
couttskate <katecou@pnghs.school.nz>
2022-05-04 13:42:20 +1200

    Update 12_surcharge.py

--- 12_surcharge.py
+++ 12_surcharge.py
@@ -1,4 +1,5 @@
-import ...
+import pandas
+import re
 
 # Function goes here
 # WARNING: The response is returned in Title Case
@@ -13,8 +14,8 @@ def string_check(choice, options):
         #if the snack is in one of the lists, return the full
         if choice in var_list:
 
-            #get full name of the snack and put it
-            #in title case so it looks nice when outputted
+            # get full name of the snack and put it
+            # in title case so it looks nice when outputted
             chosen = var_list[0].title()
             is_valid = "yes"
             break
@@ -30,3 +31,37 @@ def string_check(choice, options):
         print("Please enter a valid option")
         print()
         return "invalid choice"
+
+
+# Main Routine
+
+pay_method = [
+    ["cash", "ca"],
+    ["credit","cr"]
+]
+
+# loop until exit code...
+name = ""
+while name != "xxx":
+    name = input("Name: ")
+    if name == "xxx":
+        break
+
+    # ask for payment method
+    how_pay = "invalid choice"
+    while how_pay == "invalid choice":
+        how_pay = input("Please choose a payment method (cash / credit)").lower()
+        how_pay = string_check(how_pay, pay_method)
+
+    # ask for subtotal (for testing purposes)
+    subtotal = float(input("Sub total? $"))
+
+    if how_pay == "Credit":
+        surcharge = 0.05 * subtotal
+    else:
+        surcharge = 0
+
+    total = subtotal + surcharge
+
+    print("Name: {} | Subtotal ${:.2f} | Surcharge ${:.2f} | "
+          "Total Payable: ${:.2f}".format(name, subtotal, surcharge, total))
