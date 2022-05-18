1a2c5f5ddc0825558e6ab6bfbdc2fdfba3f586f3
couttskate <katecou@pnghs.school.nz>
2022-05-18 13:57:09 +1200

    Update 00_MMF_base_v6.py

--- 00_MMF_base_v6.py
+++ 00_MMF_base_v6.py
@@ -202,6 +202,12 @@ snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
 # Store surcharge multiplier
 surcharge_mult_list = []
 
+# Lists to store summary data...
+summary_headings = ["Popcorn", "M&M's", "Pita Chips", "Water",
+                    "Orange Juice", "Snack Profit", "Ticket Profit",
+                    "Total Profit"]
+summary_data = []
+
 # Data Frame Dictionary
 movie_data_dict = {
     'Name': all_names,
@@ -213,6 +219,12 @@ movie_data_dict = {
     'Orange Juice': orange_juice,
     'Surcharge_Multiplier': surcharge_mult_list
 
+}
+
+# Summary Dictionary
+summary_data_dict = {
+    'Item': summary_headings,
+    'Amount': summary_data
 }
 
 # Cost of each snack
@@ -326,23 +338,48 @@ movie_frame = movie_frame.rename(columns={'Orange Juice': 'OJ',
                                             'Pita Chips':'Chips',
                                             'Surcharge_Multiplier':'SM'})
 
+# Set up summary dataframe
+# populate snack items...
+for item in snack_lists:
+    # sum items in each snack list
+    summary_data.append(sum(item))
+
+# Get snack profit
+# Get snack total from panda
+snack_total = movie_frame['Snacks'].sum()
+snack_profit = snack_total * 0.2
+summary_data.append(snack_profit)
+
+# Get ticket profit and add to list
+ticket_profit = ticket_sales - (5 * ticket_count)
+summary_data.append(ticket_profit)
+
+# Work out total profit and add to list
+total_profit = snack_profit + ticket_profit
+summary_data.append(total_profit)
+
+# Create summary frame
+summary_frame = pandas.DataFrame(summary_data_dict)
+summary_frame = summary_frame.set_index('Item')
+
 # Set up columns to be printed ...
 pandas.set_option('display.max_columns', None)
 
 # Display numbers to 2 dp...
 pandas.set_option('display.precision', 2)
 
-print_all = input("print all columns?? (y) for yes ")
-if print_all == "y":
-    print(movie_frame)
-else:
-    print(movie_frame[['Ticket', 'Snacks', 'Sub Total', 'Surcharge', 'Total']])
+print()
+print("*** Ticket / Snack Information ***")
+print("Note: for full details, please see the excel file called")
+print()
+print(movie_frame[['Ticket', 'Snacks', 'Sub Total',
+                   'Surcharge', 'Total']])
 
 print()
 
-# Calculate ticket profit...
-ticket_profit = ticket_sales - (5 * ticket_count)
-print("Ticket profit: ${:.2f}".format(ticket_profit))
+print("*** Snack / Profit Summary ***")
+print()
+print(summary_frame)
 
 # Tell user if they have unsold tickets
 if ticket_count < MAX_TICKETS:
